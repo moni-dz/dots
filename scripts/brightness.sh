ffe378cbdda8b2833847023cd92b266654126911
fortuneteller2k <lythe1107@gmail.com>
2021-01-22 16:27:05 +0800

    remove unneeded scripts

--- scripts/brightness.sh
+++
@@ -1,8 +0,0 @@
-if [ "$1" = "up" ]; then
-    xbacklight -inc 10
-elif [ "$1" = "down" ]; then
-    xbacklight -dec 10
-else
-    echo 'Usage: brightness <up/down>'
-    exit 0;
-fi
--- scripts/locker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/run/current-system/sw/bin/xss-lock slock
-/run/current-system/sw/bin/xidlehook --not-when-fullscreen --not-when-audio --timer 600 slock
